167d6ee627599f62e6f812dfe058491eeeaf763b
Hamzah Qureshi <hamzahqureshi25@gmail.com>
2020-10-09 22:23:32 +0100

    Finished Day 4, Part 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -2,14 +2,24 @@ def CheckValid(code):
     """Used to validate codes
     Input: STR - since you can compare characters in strings
     Output: INT (0,1) - will update counter if valid"""
-    i = 0
-    while i<5:
+    doubles = 0
+    for i in range(5):
         if code[i] == code[i+1]:
-            for j in range(5):
-                if int(code[j])>int(code[j+1]):
-                    return 0
-            return 1
-        i += 1
+            try:
+                if code[i] == code[i+2] or code[i] == code[i-1]:
+                    pass
+                else:
+                    doubles += 1
+            except:
+                if code[i] == code[i-1]:
+                    pass
+                else:
+                    doubles += 1
+    if doubles > 0:
+        for j in range(5):
+            if int(code[j])>int(code[j+1]):
+                return 0
+        return 1    
     return 0
 
 def CodesToCheck(start, end):
