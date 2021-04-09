2654f1bad8ecd9a131c1f53e04a579d1daaaec7c
Hamzah Qureshi <hamzahqureshi25@gmail.com>
2021-04-09 18:05:00 +0100

    Finished Day 8 Part 2

--- 2020/Day8/day8.py
+++ 2020/Day8/day8.py
@@ -7,13 +7,15 @@ def ReadFile(FileName):
             Instructions.append(templist)
     return Instructions
 
-def RepeatUntilDouble(Instructions):
+def RepeatUntilDoubleOrEnd(Instructions):
     pointer = 0
     visited = []
     accumulator = 0
     while True:
         if pointer in visited:
-            return accumulator
+            return False, accumulator
+        elif pointer >= len(Instructions):
+            return True, accumulator
         visited.append(pointer)
         current_instruction = Instructions[pointer]
         if current_instruction[0] == "nop":
@@ -24,9 +26,33 @@ def RepeatUntilDouble(Instructions):
             accumulator += current_instruction[1]
             pointer += 1
 
+def FindFixedCode(Instructions):
+    Passed = False
+    JmpNopCount = 0
+    while not Passed:
+        count = 0
+        changed = False
+        ModifiedInstructions = []
+        for item in Instructions:
+            if item[0] in ["jmp","nop"] and count>=JmpNopCount and not changed:
+                tempitem = item[:]
+                if item[0] == "jmp":
+                    tempitem[0] = "nop"
+                elif item[0] ==  "nop":
+                    tempitem[0] = "jmp"
+                ModifiedInstructions.append(tempitem)
+                changed = True
+                JmpNopCount += 1
+            elif item[0] in ["jmp","nop"] and not changed:
+                ModifiedInstructions.append(item)
+                count += 1
+            else:
+                ModifiedInstructions.append(item)
+        Passed, accumulator = RepeatUntilDoubleOrEnd(ModifiedInstructions)
+    return accumulator
 
 def Main():
-    print(RepeatUntilDouble(ReadFile("2020/Day8/main_input.txt")))
+    print(FindFixedCode(ReadFile("2020/Day8/main_input.txt")))
 
 if __name__ == "__main__":
     Main()
