eb93220f894f9203144997c3e68cc2471063929b
Hamzah Qureshi <hamzahqureshi25@gmail.com>
2021-04-10 17:08:56 +0100

    Finished Day 10 Part 2

--- 2020/Day10/day10.py
+++ 2020/Day10/day10.py
@@ -20,8 +20,62 @@ def ThreeAndOneProduct(JoltageList):
 def FindDifference(Number1, Number2):
     return Number2 - Number1
 
+def FindAllLinks(Number, neighbours, Target):
+    global LinkCount
+    if Number == Target:
+        LinkCount += 1
+    else:
+        for x in neighbours:
+            if x[0] == Number:
+                links = x[1]
+        for item in links:
+            FindAllLinks(item, neighbours, Target)
+
+def SplitIntoNeighbours(JoltageList):
+    NeighbourList = []
+    for item in JoltageList:
+        TempNeighbours = []
+        if JoltageList[JoltageList.index(item)] == JoltageList[-1]:
+            break
+        i = 1
+        while JoltageList[JoltageList.index(item)+i] - JoltageList[JoltageList.index(item)] < 4 and JoltageList[JoltageList.index(item)+i] - JoltageList[JoltageList.index(item)] > 0:
+            TempNeighbours.append(JoltageList[JoltageList.index(item)+i])
+            if JoltageList[JoltageList.index(item)+i] == JoltageList[-1]:
+                break
+            i += 1
+        NeighbourList.append([JoltageList[JoltageList.index(item)],TempNeighbours])
+    return NeighbourList
+
+def CompressNeighbours(NeighbourList):
+    complist = []
+    templist = []
+    maxnum = -1
+    for item in NeighbourList:
+        if item[0] == maxnum:
+            if templist:
+                complist.append(templist[:])
+                templist.clear()
+        elif len(item[1]) > 1 or item[0]<maxnum:
+            if max(item[1]) > maxnum:
+                maxnum = max(item[1])
+                templist.append(item)
+            else:
+                templist.append(item)
+    if templist:
+        complist.append(templist[:])
+        templist.clear()
+    return complist
+
 def Main():
-    print(ThreeAndOneProduct(ReadFile("2020/Day10/main_input.txt")))
+    global LinkCount
+    NList = SplitIntoNeighbours(ReadFile("2020/Day10/main_input.txt"))
+    answer = 1
+    for item in CompressNeighbours(NList):
+        LinkCount = 0
+        FindAllLinks(item[0][0], item, item[-1][1][0])
+        answer = answer * LinkCount
+    print(answer)
 
 if __name__ == "__main__":
+    LinkCount = 0
     Main()
